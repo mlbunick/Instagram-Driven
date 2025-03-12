4695a3668604cb6390284c8dbc566b980d1fc20a
Matheus <mlbunick85@gmail.com>
2025-03-11 21:58:45 -0300

    Componente Posts | Renderização

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from "./Navbar";
 import Stories from "./Stories";
+import Posts from "./Posts";
 export default function App() {
   return (
     <>
         <Navbar/>
         <Stories/>
+        <Posts/>
     </>
   );
 }
