dcd54338da779d1d49414e651f9f9ac05fad3bc4
Matheus <mlbunick85@gmail.com>
2025-03-11 21:25:06 -0300

    Componente Stories | Inicial

---
+++ src/Stories.js
@@ -0,0 +1,36 @@
+export default function Stories(){
+    const stories = [
+        {img: "/assets/img/9gag.svg", usuario: "9gag" },
+        {img: "/assets/img/meowed.svg", usuario: "meowed" },
+        {img: "/assets/img/barked.svg", usuario: "barked" },
+        {img: "/assets/img/nathanwpylestrangeplanet.svg", usuario: "nathanwpylestrangeplanet" },
+        {img: "/assets/img/wawawicomics.svg", usuario: "wawawicomics" },
+        {img: "/assets/img/respondeai.svg", usuario: "respondeai" },
+        {img: "/assets/img/filomoderna.svg", usuario: "filomoderna" },
+        {img: "/assets/img/memeriagourmet.svg", usuario: "memeriagourmet" }
+    ];
+
+    return(
+        <div class="stories">
+            
+            {stories.map(story => <Story imagem={stories.imagem} usuario={stories.usuario}/>)}
+            
+            <div class="setinha">
+                <ion-icon name="chevron-forward-circle"></ion-icon>
+            </div>
+        </div>
+    )
+}
+
+function Story(props){
+    return(
+        <div class="story">
+            <div class="imagem">
+              <img src={props.imagem} alt={props.usuario}/>
+            </div>
+            <div class="usuario">
+                {props.usuario}
+            </div>
+        </div>
+    )
+}
